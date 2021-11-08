84adbd5dc80094ef41819ca84c81598d06db452f
Richard Hodges <hodges.r@gmail.com>
2021-11-08 17:10:43 +0100

    wip

--- atomic_group.cpp
+++ atomic_group.cpp
@@ -11,6 +11,7 @@
 #include <asio/experimental/append.hpp>
 #include <asio/experimental/deferred.hpp>
 #include <asioex/error_code.hpp>
+#include <asioex/concepts/transfer_latch.hpp>
 
 #include <iostream>
 #include <memory>
@@ -47,7 +48,8 @@ struct story_op : asio::coroutine
         , current(std::begin(lines))
         {
         }
-        asio::steady_timer                       timer;
+        asio::basic_waitable_timer < std::chrono::steady_clock,
+            asio::wait_traits< std::chrono::steady_clock >, Executor> timer;
         std::span< std::string const >::iterator current;
     };
 
@@ -144,14 +146,133 @@ struct story_op : asio::coroutine
     }
 };
 
+template < asioex::concepts::transfer_latch Latch, class Executor, class Handler >
+struct latched_story_op : asio::coroutine
+{
+    Latch *                               latch_;
+    asio::executor_work_guard< Executor > wg_;
+    Handler                               handler_;
+
+    struct state_t
+    {
+        state_t(asio::any_io_executor                 exec,
+                std::span< std::string const > const &lines)
+        : timer(std::move(exec))
+        , current(std::begin(lines))
+        {
+        }
+        asio::steady_timer                       timer;
+        std::span< std::string const >::iterator current;
+    };
+
+    std::unique_ptr< state_t >     pstate;
+    std::span< std::string const > lines;
+    int                            yielded = 0;
+
+    latched_story_op(Latch* latch, Executor e, Handler h, std::span< std::string const > lines)
+    : latch_(latch)
+    , wg_(std::move(e))
+    , handler_(std::move(h))
+    , pstate()
+    , lines(lines)
+    {
+    }
+
+    using allocator_type = asio::associated_allocator_t< Handler >;
+
+    using executor_type = Executor;
+
+    allocator_type
+    get_allocator() const
+    {
+        return asio::get_associated_allocator(handler_);
+    }
+
+    executor_type
+    get_executor() const
+    {
+        return wg_.get_executor();
+    }
+
+#include <asio/yield.hpp>
+    void operator()(asioex::error_code ec = {})
+    {
+        using asio::experimental::append;
+        using namespace std::literals;
+
+        reenter(this) for (;;)
+        {
+            if (!allocate_state(ec))
+                return complete(ec);
+
+            while (pstate->current != std::end(lines))
+            {
+                if (pstate->current != std::begin(lines))
+                {
+                    pstate->timer.expires_after(500ms);
+                    ++yielded;
+                    yield pstate->timer.async_wait(std::move(*this));
+                    if (ec)
+                        break;
+                }
+                println(*pstate->current++);
+            }
+
+            return complete(ec);
+        }
+    }
+#include <asio/unyield.hpp>
+
+    bool
+    allocate_state(asioex::error_code &ec)
+    {
+        try
+        {
+            pstate = std::make_unique< state_t >(wg_.get_executor(), lines);
+            ec.clear();
+        }
+        catch (std::bad_alloc &)
+        {
+            ec = asio::error::no_memory;
+        }
+        catch (std::exception &e)
+        {
+            ec = asio::error::invalid_argument;
+        }
+        return !ec;
+    }
+
+    void
+    complete(asioex::error_code ec)
+    {
+        using asio::experimental::append;
+
+        pstate.reset();
+
+        if (yielded)
+            return handler_(ec);
+        else
+        {
+            auto e = get_executor();
+            asio::post(e, append(std::move(handler_), ec));
+        }
+    }
+};
+
 struct initiate_story
 {
     template < class Handler >
     void
     operator()(Handler&& handler, std::span< std::string const > lines) const
     {
-        using exec_type = asio::associated_executor_t<Handler, asio::any_io_executor>;
-        auto exec       = asio::get_associated_executor(handler, asio::any_io_executor());
+        using exec_type = asio::associated_executor_t<
+            Handler, 
+            asio::any_io_executor>;
+
+        auto exec = asio::get_associated_executor(
+            handler, 
+            asio::any_io_executor());
+
         auto op = story_op< exec_type, Handler >(
             std::move(exec), 
             std::move(handler), 
